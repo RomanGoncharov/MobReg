cbae47ba2c649d13a459543ecff90a98118ccade
RomanUsynin <r.usynin@crystalnix.com>
2015-05-13 03:22:59 +0600

    Added validation for form.

--- mobreg/src/main/java/com/romanusynin/mobreg/mobreg/activities/TakeTicketActivity.java
+++ mobreg/src/main/java/com/romanusynin/mobreg/mobreg/activities/TakeTicketActivity.java
@@ -11,10 +11,16 @@ import com.romanusynin.mobreg.mobreg.R;
 import com.romanusynin.mobreg.mobreg.TextValidator;
 import com.romanusynin.mobreg.mobreg.objects.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TakeTicketActivity extends Activity{
     private Department department;
     private Doctor doctor;
     private Ticket ticket;
+    private Button continueButton;
+    private EditText numberPolicyField;
+    private EditText dateBirthField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,34 +30,54 @@ public class TakeTicketActivity extends Activity{
 //        department = (Department) intent.getExtras().getSerializable("department");
 //        doctor = (Doctor) intent.getExtras().getSerializable("doctor");
 //        ticket = (Ticket) intent.getExtras().getSerializable("ticket");
-        EditText numberPolicyField = (EditText) findViewById(R.id.policy_field);
+
+        numberPolicyField = (EditText) findViewById(R.id.policy_field);
         numberPolicyField.addTextChangedListener(new TextValidator(numberPolicyField) {
             @Override
-            public void validate(EditText textView, String text) {
+            public void validate(EditText editText, String text) {
                 int numberPolicyLength = text.length();
                 if (numberPolicyLength == 0) {
-                    textView.setError("Это поле обязательное");
+                    editText.setError("Это поле обязательное");
+                    continueButton.setEnabled(false);
                 } else if (numberPolicyLength < 6) {
-                    textView.setError("Длина не должна быть меньше 6 цифр");
+                    editText.setError("Длина не должна быть меньше 6 цифр");
+                    continueButton.setEnabled(false);
                 } else if (numberPolicyLength > 16) {
-                    textView.setError("Длина не должна превышать 16 цифр");
-                } else textView.setError(null);
+                    editText.setError("Длина не должна превышать 16 цифр");
+                    continueButton.setEnabled(false);
+                } else {
+                    editText.setError(null);
+                    if (dateBirthField.getError()==null && dateBirthField.getText().length() > 0)
+                        continueButton.setEnabled(true);
+                }
             }
         });
 
-        EditText dateBirthField = (EditText) findViewById(R.id.date_birth_field);
-        dateBirthField.addTextChangedListener(new TextValidator(numberPolicyField) {
+        dateBirthField = (EditText) findViewById(R.id.date_birth_field);
+        dateBirthField.addTextChangedListener(new TextValidator(dateBirthField) {
             @Override
-            public void validate(EditText textView, String text) {
-                // add validate for dateField
+            public void validate(EditText editText, String text) {
+                Pattern p = Pattern.compile("^(0[1-9]|[12][0-9]|3[01])\\.(0[1-9]|1[012])\\.(19|20)\\d\\d$");
+                Matcher m = p.matcher(text);
+                if (!m.matches()) {
+                    editText.setError("Введите дату в формате dd.mm.yyyy");
+                    continueButton.setEnabled(false);
+                } else {
+                    editText.setError(null);
+                    if (numberPolicyField.getError()==null && numberPolicyField.getText().length() > 0)
+                        continueButton.setEnabled(true);
+                }
             }
         });
-        Button continueButton = (Button) findViewById(R.id.continueButton);
+
+        continueButton = (Button) findViewById(R.id.continueButton);
+        continueButton.setEnabled(false);
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
             }
         });
+
+
     }
 }
